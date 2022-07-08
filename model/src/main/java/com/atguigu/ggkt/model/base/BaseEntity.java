03b52a463cb2338c612e73ff56b003cdaf79cb5a
gududehou <2783710964@qq.com>
2022-07-08 19:24:00 +0800

    配置Swagger

--- model/src/main/java/com/atguigu/ggkt/model/base/BaseEntity.java
+++ model/src/main/java/com/atguigu/ggkt/model/base/BaseEntity.java
@@ -39,5 +39,5 @@ public class BaseEntity implements Serializable {
 
     @ApiModelProperty(value = "其他参数")
     @TableField(exist = false)
-    private Map<String,Object> param = new HashMap<>();
+    private Map<String, Object> param = new HashMap<>();
 }
