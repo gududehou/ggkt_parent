39a183bd1ddd745de8efc956e92e03f310a5ef3b
gududehou <2783710964@qq.com>
2022-07-09 11:15:09 +0800

    全局异常处理

--- common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
+++ common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.atguigu.ggkt.exception;
 
 import com.atguigu.ggkt.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -29,12 +30,16 @@ public class GlobalExceptionHandler {
             log.error("数据校验异常{}", e.getMessage());
             return Result.fail(message);
         }
+        if (e instanceof DuplicateKeyException) {
+            log.error("数据重复异常{}", e.getMessage());
+            return Result.fail("数据重复啦");
+        }
         //业务异常
         if (e instanceof GgktException) {
             log.error("业务异常{}", ((GgktException) e).getMsg());
             return Result.fail(((GgktException) e).getMsg());
         }
         e.printStackTrace();
-        return Result.fail().message("执行了全局异常处理");
+        return Result.fail("全局异常处理");
     }
 }
