5b8b78582faf1a8334ee95a3ccd980f0af9b6230
gududehou <2783710964@qq.com>
2022-07-09 08:47:37 +0800

    分页插件

--- common/service_utils/src/main/java/com/atguigu/ggkt/config/MybatisPlusConfig.java
+++ common/service_utils/src/main/java/com/atguigu/ggkt/config/MybatisPlusConfig.java
@@ -1,16 +1,11 @@
-package com.atguigu.ggkt.vod.config;
+package com.atguigu.ggkt.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * 配置类
- */
 @Configuration
-@MapperScan("com.atguigu.ggkt.vod.mapper")
-public class VodConfig {
+public class MybatisPlusConfig {
     /**
      * 分页插件
      */
--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
@@ -1,10 +1,12 @@
 package com.atguigu.ggkt.vod;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.atguigu.ggkt.vod.mapper")
 @ComponentScan("com.atguigu")
 public class ServiceVodApplication {
     public static void main(String[] args) {
